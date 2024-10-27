614a2b063e883387c6d43342c139331076467c54
Joseph Owonvwon <joseph.owonwo@gmail.com>
2024-10-27 02:28:26 +0100

    trying out Gen2 environment global

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,6 +1,10 @@
 export default eventHandler((event) => {
   const config = useRuntimeConfig(event);
-  console.log("Before", JSON.stringify(process.env), JSON.stringify(config));
+  console.log("Before", JSON.stringify(process.env),
+    // @ts-expect-error
+    env,
+    JSON.stringify(config)
+  );
 
   return "Start by editing <code>server/routes/index.ts</code>.";
 });
